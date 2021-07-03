d183bee238d2f7c22f39b12a1c85aa63ad88703c
Marcelo de Bittencourt <tchelopoa@gmail.com>
2021-07-03 20:10:26 -0300

    Adicionados dois testes para a classe Cliente

--- src/test/java/tests/ClienteTest.java
+++ src/test/java/tests/ClienteTest.java
@@ -1,4 +1,24 @@
 package tests;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import principal.Cliente;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class ClienteTest {
+
+    @Disabled
+    @Test
+    void TestaNomeCliente_ValidaNomeCorreto() {
+        Cliente clienteVip = new Cliente("Fulano de Tal");
+        assertTrue("Fulano de Tal" == clienteVip.getNome());
+    }
+
+    @Test
+    void TestaNomeCliente_ValidaNomeCorretoComAssertJ() {
+        Cliente clienteVip = new Cliente("Fulano de Tal");
+        assertThat(clienteVip.getNome()).isEqualTo("Fulano de Tal");
+    }
 }
